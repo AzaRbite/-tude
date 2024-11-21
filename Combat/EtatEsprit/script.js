2773bd9cdbad7a2e8ecca483621af23376c9d3b1
AzaRbite <maximemolliex05@gmail.com>
2024-11-20 20:43:10 -0500

    Update script.js

--- Combat/EtatEsprit/script.js
+++ Combat/EtatEsprit/script.js
@@ -1,129 +1,43 @@
 const questions = [
-    {
-        question: "Quelle couleur correspond à 'Inconscience du danger'?",
-        choices: ["Blanc", "Jaune", "Rouge", "Orange", "Noir"],
-        correct: "Blanc"
-    },
-    {
-        question: "Quel est l'état d'esprit correspondant à la description : 'Détendu et vigilant'?",
-        choices: ["Jaune", "Blanc", "Rouge", "Orange", "Noir"],
-        correct: "Jaune"
-    },
-    {
-        question: "Quel état d'esprit est associé à la couleur 'Rouge'?",
-        choices: ["Lutte", "Inconscience du danger", "Panique", "Détendu", "Préparation au danger"],
-        correct: "Lutte"
-    },
-    {
-        question: "Quelle couleur est associée à 'Préparation au danger'?",
-        choices: ["Orange", "Rouge", "Jaune", "Blanc", "Noir"],
-        correct: "Orange"
-    },
-    {
-        question: "Que signifie l'état d'esprit 'Noir'?",
-        choices: ["Panique", "Préparation au danger", "Détendu", "Lutte", "Inconscience du danger"],
-        correct: "Panique"
-    },
-    {
-        question: "Quel est l'état d'esprit pour 'Réaction en vue de maîtriser la menace'?",
-        choices: ["Rouge", "Orange", "Noir", "Jaune", "Blanc"],
-        correct: "Rouge"
-    },
-    {
-        question: "Quelle couleur symbolise la 'Lutte'?",
-        choices: ["Rouge", "Jaune", "Blanc", "Noir", "Orange"],
-        correct: "Rouge"
-    },
-    {
-        question: "Quel est le nom associé à la couleur 'Blanc'?",
-        choices: ["Inconscience du danger", "Détendu", "Préparation au danger", "Lutte", "Panique"],
-        correct: "Inconscience du danger"
-    },
-    {
-        question: "Quelle est la description pour 'Orange'?",
-        choices: ["Préparation au danger", "Panique", "Inconscience du danger", "Lutte", "Détendu"],
-        correct: "Préparation au danger"
-    },
-    {
-        question: "Quel est l'état d'esprit pour 'Panique'?",
-        choices: ["Noir", "Rouge", "Orange", "Jaune", "Blanc"],
-        correct: "Noir"
-    },
-    {
-        question: "Quelle couleur associez-vous à la vigilance accrue?",
-        choices: ["Noir", "Jaune", "Orange", "Blanc", "Rouge"],
-        correct: "Jaune"
-    },
-    {
-        question: "Que représente l'état d'esprit 'Rouge' dans un contexte de danger?",
-        choices: ["Action", "Évasion", "Alerte", "Inaction", "Calme"],
-        correct: "Action"
-    },
-    {
-        question: "Quel état d'esprit implique une préparation mentale et physique au danger?",
-        choices: ["Orange", "Blanc", "Jaune", "Noir", "Rouge"],
-        correct: "Orange"
-    },
-    {
-        question: "Quel est l'état d'esprit associé à l'insouciance?",
-        choices: ["Blanc", "Rouge", "Jaune", "Orange", "Noir"],
-        correct: "Blanc"
-    },
-    {
-        question: "Quel état d'esprit devez-vous éviter dans une situation de crise?",
-        choices: ["Noir", "Jaune", "Orange", "Blanc", "Rouge"],
-        correct: "Noir"
-    },
-    {
-        question: "Quel est l'état d'esprit pour 'Approche stratégique'?",
-        choices: ["Orange", "Rouge", "Noir", "Jaune", "Blanc"],
-        correct: "Orange"
-    },
-    {
-        question: "Quelle couleur représente 'Détente' dans une situation de stress?",
-        choices: ["Blanc", "Noir", "Jaune", "Rouge", "Orange"],
-        correct: "Blanc"
-    },
-    {
-        question: "Quel état d'esprit traduit une 'Alerte silencieuse'?",
-        choices: ["Jaune", "Noir", "Orange", "Rouge", "Blanc"],
-        correct: "Jaune"
-    },
-    {
-        question: "Que signifie la couleur 'Rouge' dans un environnement d'apprentissage?",
-        choices: ["Lutte", "Surveillance", "Relâchement", "Méditation", "Préparation"],
-        correct: "Lutte"
-    },
-    {
-        question: "Quel état d'esprit est évoqué par une 'Alerte rouge'?",
-        choices: ["Rouge", "Orange", "Jaune", "Noir", "Blanc"],
-        correct: "Rouge"
-    },
-    {
-        question: "Quelle couleur est un symbole de 'Préparation intense'?",
-        choices: ["Orange", "Rouge", "Jaune", "Blanc", "Noir"],
-        correct: "Orange"
-    },
-    {
-        question: "Quel état d'esprit est associé à la 'Réaction instinctive'?",
-        choices: ["Rouge", "Jaune", "Orange", "Blanc", "Noir"],
-        correct: "Rouge"
-    },
-    {
-        question: "Quelle couleur représente un 'État méditatif'?",
-        choices: ["Blanc", "Noir", "Jaune", "Rouge", "Orange"],
-        correct: "Blanc"
-    },
-    {
-        question: "Quel état d'esprit est lié à une 'Préparation mentale'?",
-        choices: ["Orange", "Rouge", "Jaune", "Noir", "Blanc"],
-        correct: "Orange"
-    },
-    {
-        question: "Quelle couleur incarne une 'Alerte élevée'?",
-        choices: ["Jaune", "Noir", "Orange", "Rouge", "Blanc"],
-        correct: "Jaune"
-    }
+    // Blanc
+    { question: "Quel est le nom associé à la couleur 'Blanc' ?", choices: ["Inconscience du danger", "Détendu et vigilant", "Préparation au danger", "Lutte", "Panique"], correct: "Inconscience du danger" },
+    { question: "Quelle est la description pour la couleur 'Blanc' ?", choices: ["Aucunement préoccupé de ce qui se passe dans le milieu environnant.", "C'est cette attitude que l'agent doit adopter au travail.", "Réaction en vue de maîtriser la menace de danger immédiat.", "Désordre, désorganisation, perte de moyens.", "C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions."], correct: "Aucunement préoccupé de ce qui se passe dans le milieu environnant." },
+    { question: "Quelle couleur est associée à cette description : 'Aucunement préoccupé de ce qui se passe dans le milieu environnant.' ?", choices: ["Blanc", "Jaune", "Orange", "Rouge", "Noir"], correct: "Blanc" },
+    { question: "Quel est le nom pour la description 'Aucunement préoccupé de ce qui se passe dans le milieu environnant.' ?", choices: ["Inconscience du danger", "Détendu et vigilant", "Préparation au danger", "Lutte", "Panique"], correct: "Inconscience du danger" },
+    { question: "Quelle couleur correspond à 'Inconscience du danger' ?", choices: ["Blanc", "Jaune", "Orange", "Rouge", "Noir"], correct: "Blanc" },
+    { question: "Quelle est la description pour 'Inconscience du danger' ?", choices: ["Aucunement préoccupé de ce qui se passe dans le milieu environnant.", "C'est cette attitude que l'agent doit adopter au travail.", "Réaction en vue de maîtriser la menace de danger immédiat.", "Désordre, désorganisation, perte de moyens.", "C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions."], correct: "Aucunement préoccupé de ce qui se passe dans le milieu environnant." },
+
+    // Jaune
+    { question: "Quel est le nom associé à la couleur 'Jaune' ?", choices: ["Détendu et vigilant", "Inconscience du danger", "Préparation au danger", "Lutte", "Panique"], correct: "Détendu et vigilant" },
+    { question: "Quelle est la description pour la couleur 'Jaune' ?", choices: ["C'est cette attitude que l'agent doit adopter au travail. Pas de méfiance maladive, simplement une prise de conscience de l'environnement et un esprit éveillé, capable de détecter les premiers signes de danger.", "Aucunement préoccupé de ce qui se passe dans le milieu environnant.", "Réaction en vue de maîtriser la menace de danger immédiat.", "Désordre, désorganisation, perte de moyens.", "C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions."], correct: "C'est cette attitude que l'agent doit adopter au travail. Pas de méfiance maladive, simplement une prise de conscience de l'environnement et un esprit éveillé, capable de détecter les premiers signes de danger." },
+    { question: "Quelle couleur est associée à cette description : 'C'est cette attitude que l'agent doit adopter au travail. Pas de méfiance maladive, simplement une prise de conscience de l'environnement et un esprit éveillé, capable de détecter les premiers signes de danger.' ?", choices: ["Jaune", "Blanc", "Orange", "Rouge", "Noir"], correct: "Jaune" },
+    { question: "Quel est le nom pour la description 'C'est cette attitude que l'agent doit adopter au travail. Pas de méfiance maladive, simplement une prise de conscience de l'environnement et un esprit éveillé, capable de détecter les premiers signes de danger.' ?", choices: ["Détendu et vigilant", "Inconscience du danger", "Préparation au danger", "Lutte", "Panique"], correct: "Détendu et vigilant" },
+    { question: "Quelle couleur correspond à 'Détendu et vigilant' ?", choices: ["Jaune", "Blanc", "Orange", "Rouge", "Noir"], correct: "Jaune" },
+    { question: "Quelle est la description pour 'Détendu et vigilant' ?", choices: ["C'est cette attitude que l'agent doit adopter au travail. Pas de méfiance maladive, simplement une prise de conscience de l'environnement et un esprit éveillé, capable de détecter les premiers signes de danger.", "Aucunement préoccupé de ce qui se passe dans le milieu environnant.", "Réaction en vue de maîtriser la menace de danger immédiat.", "Désordre, désorganisation, perte de moyens.", "C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions."], correct: "C'est cette attitude que l'agent doit adopter au travail. Pas de méfiance maladive, simplement une prise de conscience de l'environnement et un esprit éveillé, capable de détecter les premiers signes de danger." },
+
+    // Orange
+    { question: "Quel est le nom associé à la couleur 'Orange' ?", choices: ["Préparation au danger", "Inconscience du danger", "Détendu et vigilant", "Lutte", "Panique"], correct: "Préparation au danger" },
+    { question: "Quelle est la description pour la couleur 'Orange' ?", choices: ["C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions nécessaires pour faire face au danger éventuel dont il a perçu les signes", "C'est cette attitude que l'agent doit adopter au travail.", "Réaction en vue de maîtriser la menace de danger immédiat.", "Désordre, désorganisation, perte de moyens.", "Aucunement préoccupé de ce qui se passe dans le milieu environnant."], correct: "C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions nécessaires pour faire face au danger éventuel dont il a perçu les signes" },
+    { question: "Quelle couleur est associée à cette description : 'C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions nécessaires pour faire face au danger éventuel dont il a perçu les signes' ?", choices: ["Orange", "Jaune", "Blanc", "Rouge", "Noir"], correct: "Orange" },
+    { question: "Quel est le nom pour la description 'C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions nécessaires pour faire face au danger éventuel dont il a perçu les signes' ?", choices: ["Préparation au danger", "Inconscience du danger", "Détendu et vigilant", "Lutte", "Panique"], correct: "Préparation au danger" },
+    { question: "Quelle couleur correspond à 'Préparation au danger' ?", choices: ["Orange", "Jaune", "Blanc", "Rouge", "Noir"], correct: "Orange" },
+    { question: "Quelle est la description pour 'Préparation au danger' ?", choices: ["C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions nécessaires pour faire face au danger éventuel dont il a perçu les signes", "C'est cette attitude que l'agent doit adopter au travail.", "Réaction en vue de maîtriser la menace de danger immédiat.", "Désordre, désorganisation, perte de moyens.", "Aucunement préoccupé de ce qui se passe dans le milieu environnant."], correct: "C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions nécessaires pour faire face au danger éventuel dont il a perçu les signes" },
+
+    // Rouge
+    { question: "Quel est le nom associé à la couleur 'Rouge' ?", choices: ["Lutte", "Inconscience du danger", "Détendu et vigilant", "Préparation au danger", "Panique"], correct: "Lutte" },
+    { question: "Quelle est la description pour la couleur 'Rouge' ?", choices: ["Réaction en vue de maîtriser la menace de danger immédiat et vigilance pour prévenir d'autres dangers possibles.", "C'est cette attitude que l'agent doit adopter au travail.", "Désordre, désorganisation, perte de moyens.", "Aucunement préoccupé de ce qui se passe dans le milieu environnant.", "C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions."], correct: "Réaction en vue de maîtriser la menace de danger immédiat et vigilance pour prévenir d'autres dangers possibles." },
+    { question: "Quelle couleur est associée à cette description : 'Réaction en vue de maîtriser la menace de danger immédiat et vigilance pour prévenir d'autres dangers possibles.' ?", choices: ["Rouge", "Jaune", "Orange", "Blanc", "Noir"], correct: "Rouge" },
+    { question: "Quel est le nom pour la description 'Réaction en vue de maîtriser la menace de danger immédiat et vigilance pour prévenir d'autres dangers possibles.' ?", choices: ["Lutte", "Inconscience du danger", "Détendu et vigilant", "Préparation au danger", "Panique"], correct: "Lutte" },
+    { question: "Quelle couleur correspond à 'Lutte' ?", choices: ["Rouge", "Jaune", "Orange", "Blanc", "Noir"], correct: "Rouge" },
+    { question: "Quelle est la description pour 'Lutte' ?", choices: ["Réaction en vue de maîtriser la menace de danger immédiat et vigilance pour prévenir d'autres dangers possibles.", "C'est cette attitude que l'agent doit adopter au travail.", "Désordre, désorganisation, perte de moyens.", "Aucunement préoccupé de ce qui se passe dans le milieu environnant.", "C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions."], correct: "Réaction en vue de maîtriser la menace de danger immédiat et vigilance pour prévenir d'autres dangers possibles." },
+
+    // Noir
+    { question: "Quel est le nom associé à la couleur 'Noir' ?", choices: ["Panique", "Inconscience du danger", "Détendu et vigilant", "Préparation au danger", "Lutte"], correct: "Panique" },
+    { question: "Quelle est la description pour la couleur 'Noir' ?", choices: ["Désordre, désorganisation, perte de moyens.", "C'est cette attitude que l'agent doit adopter au travail.", "Réaction en vue de maîtriser la menace de danger immédiat.", "Aucunement préoccupé de ce qui se passe dans le milieu environnant.", "C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions."], correct: "Désordre, désorganisation, perte de moyens." },
+    { question: "Quelle couleur est associée à cette description : 'Désordre, désorganisation, perte de moyens.' ?", choices: ["Noir", "Jaune", "Orange", "Rouge", "Blanc"], correct: "Noir" },
+    { question: "Quel est le nom pour la description 'Désordre, désorganisation, perte de moyens.' ?", choices: ["Panique", "Inconscience du danger", "Détendu et vigilant", "Préparation au danger", "Lutte"], correct: "Panique" },
+    { question: "Quelle couleur correspond à 'Panique' ?", choices: ["Noir", "Jaune", "Orange", "Rouge", "Blanc"], correct: "Noir" },
+    { question: "Quelle est la description pour 'Panique' ?", choices: ["Désordre, désorganisation, perte de moyens.", "C'est cette attitude que l'agent doit adopter au travail.", "Réaction en vue de maîtriser la menace de danger immédiat.", "Aucunement préoccupé de ce qui se passe dans le milieu environnant.", "C'est à cette étape que l'agent doit mettre en pratique les mesures de précautions."], correct: "Désordre, désorganisation, perte de moyens." }
 ];
 
 let currentQuestionIndex = 0;
@@ -222,8 +136,8 @@ window.onload = () => {
     counterDiv.style.color = '#ffffff';
     counterDiv.style.fontSize = '1.2em';  // Ajuster la taille de la police
     counterDiv.style.fontWeight = 'bold'; // Mettre en gras
+    counterDiv.style.transform = 'translateY(-50%)'; // Pour aligner avec le header
     header.appendChild(counterDiv);
     
     showQuestion(currentQuestionIndex);
 };
-
