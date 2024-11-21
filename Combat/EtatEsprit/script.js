70c95f1bd8ca554ded41d1d0b4dfdb41c240a9f0
AzaRbite <maximemolliex05@gmail.com>
2024-11-20 21:38:38 -0500

    Update script.js

--- Combat/EtatEsprit/script.js
+++ Combat/EtatEsprit/script.js
@@ -69,10 +69,7 @@ function generateNonRepeatingOrder(questions, length) {
 }
 
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+    array.sort(() => Math.random() - 0.5);
     return array;
 }
 
