1db02bc80d792be60038c45be20ac60147f19fa9
AzaRbite <maximemolliex05@gmail.com>
2024-11-25 20:46:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Menu de base chargé avec succès. Sélectionnez un cours pour commencer.");
+    updateCurrentDate();
 });
 
+function updateCurrentDate() {
+    const calendarEl = document.getElementById('calendar');
+    const today = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = today.toLocaleDateString('fr-FR', options);
 
+    calendarEl.textContent = `Nous sommes le ${formattedDate}`;
+}
